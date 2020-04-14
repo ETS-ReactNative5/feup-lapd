8a0baccec3020db9852635f746357e1fca7f36b6
Nuno Lopes <up201605337@fe.up.pt>
2020-04-14 12:32:20 +0100

    Weather forecast endpoint done

--- server/routes/weather.js
+++ server/routes/weather.js
@@ -4,14 +4,33 @@ const { getForecast } = require("../modules/openweathermap");
 
 /* GET home page. */
 router.get("/", async function(req, res) {
-  // TODO: Get parameters from url
-  getForecast("Porto")
+
+  if(!validateWeatherRequest(res, req.query)) return;
+
+  getForecast(req.query.city)
     .then((response) => {
       res.send(response.data)
     })
     .catch((err) => {
       console.log(err)
+      res.status(500).send({
+        message: 'An error occured while getting the forecast',
+        error: err
+      })
     })
 });
+
+validateWeatherRequest = (res, query) => {
+  if(query.city === undefined){
+    res.status(400).send({
+      message: 'No city provided for weather forecast'
+    })
+    return false;
+  }
+
+  // Check if city exists ?
+
+  return true;
+}
 
 module.exports = router;
