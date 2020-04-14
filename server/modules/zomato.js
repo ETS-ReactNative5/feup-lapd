148d28da0eb7087dc265f3c345a33d820a577e4a
Nuno Lopes <up201605337@fe.up.pt>
2020-04-14 12:13:23 +0100

    Restaurants endpoint done

--- server/modules/zomato.js
+++ server/modules/zomato.js
@@ -2,7 +2,8 @@ const axios = require('axios')
 const qs = require('querystring');
 var zomato = require("../config/config.js").zomato;
 
-exports.getRestaurants = async (city) => {
+// This endpoint only returs 20 restaurants at a time, use offset parameter to get the next 20 restaurants
+exports.getRestaurants = async (city, offset, sort, order) => {
   let response = await axios({
     method: "GET",
     url: `${zomato.url}/locations?query=${city}`,
@@ -11,17 +12,24 @@ exports.getRestaurants = async (city) => {
     }
   });
 
-  let entity_id = response.data.location_suggestions[0].entity_id || null
+  let entity_id = response.data.location_suggestions[0].entity_id || null;
+  let entity_type = response.data.location_suggestions[0].entity_type || null;
 
   if(entity_id === null)
     return null;
 
-  const data = {
-    entity_type: "city",
-    entity_id: entity_id
+  // only accept cities, zones and subzones
+  if(entity_type !== "city" && entity_type !== "zone" && entity_type !== "subzone"){
+    return null;
   }
 
-  // TODO: Only shows 20 options at a time
+  const data = {
+    entity_type: entity_type,
+    entity_id: entity_id,
+    start: offset,            //fetch results after offset
+    sort: sort,               //cost, rating or real_distance
+    order: order              //asc or desc
+  }
 
   return await axios({
     method: "GET",
--- server/routes/restaurant.js
+++ server/routes/restaurant.js
@@ -4,14 +4,54 @@ const { getRestaurants } = require("../modules/zomato");
 
 /* GET home page. */
 router.get("/", async function(req, res) {
-  // TODO: Get parameters from url
-  getRestaurants("Porto")
+
+  if(!validateRestaurantsRequest(res, req.query)) return;
+
+  getRestaurants(req.query.city, req.query.offset, req.query.sort, req.query.order)
     .then((response) => {
       res.send(response.data)
     })
     .catch((err) => {
       console.log(err)
+      res.status(500).send({
+        message: 'An error occured while searching for restaurants',
+        error: err
+      })
     })
 });
+
+validateRestaurantsRequest = (res, query) => {
+  if(query.city === undefined || query.offset === undefined){
+    res.status(400).send({
+      message: 'No city and/or offset provided while searching for restaurants'
+    })
+    return false;
+  }
+
+  // Check if city exists ?
+
+  if(!/^\d+$/.test(query.offset)){
+    res.status(400).send({
+      message: 'Invalid offset parameter provided while searching for restaurants'
+    })
+    return false;
+  }
+
+  if(query.sort !== undefined && query.sort !== "cost" && query.sort !== "rating" && query.sort !== "real_distance"){
+    res.status(400).send({
+      message: 'Invalid sort parameter provided while searching for restaurants'
+    })
+    return false;
+  }
+
+  if(query.order !== undefined && query.order !== "asc" && query.order !== "desc"){
+    res.status(400).send({
+      message: 'Invalid order parameter provided while searching for restaurants'
+    })
+    return false;
+  }
+
+  return true;
+}
 
 module.exports = router;
